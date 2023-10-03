215b9511f49616d043f8bafb19e34c5c3b4fecf8
Rossemary Carballo <144977080+rcarballo08@users.noreply.github.com>
2023-10-02 21:30:24 -0600

    Add files via upload

---
+++ EjercicioNumeros20Y30YCuadrado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.numeros.pkg20.y.pkg30.y.cuadrado;
+
+/**
+ *
+ * @author HP
+ */
+public class EjercicioNumeros20Y30YCuadrado {
+    public static void main(String[] args) {
+        for (int i = 20; i <= 30; i++){
+            int valorCuadrado = i * i;
+            System.out.println("El valor del numero; " + i + " al cuadrado es: " + valorCuadrado);
+        }
+    }
+    
+}
